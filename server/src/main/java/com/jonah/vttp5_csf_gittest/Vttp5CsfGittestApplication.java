90590bb8e080b5196de714f938b7b00cad301ab9
jonahng <nyhj1998@gmail.com>
2025-03-19 10:50:20 +0800

    added gitignore

---
+++ server/src/main/java/com/jonah/vttp5_csf_gittest/Vttp5CsfGittestApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_csf_gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5CsfGittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5CsfGittestApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/jonah/vttp5_csf_gittest/Vttp5CsfGittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_csf_gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5CsfGittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
